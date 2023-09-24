85311ea597d38286e19750e0cea2969804d8858e
Full Queue Developer <FullQueueDeveloper@FullQueueDeveloper.com>
2023-09-23 20:21:23 -0400

    Updates after updating from upstream

--- keyboards/crkbd/keymaps/fullqueuedeveloper/config.h
+++ keyboards/crkbd/keymaps/fullqueuedeveloper/config.h
@@ -5,7 +5,6 @@
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
 #define MK_3_SPEED
 #define MK_C_OFFSET_0 4
 #define MK_C_OFFSET_1 32
